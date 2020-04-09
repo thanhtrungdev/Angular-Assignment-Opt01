5141b8cdc4d23205ceef4a74f54cf92b322b2852
Thanh Trung <trung9x.dev@gmail.com>
2020-04-10 00:55:15 +0700

    create service computer

--- src/app/services/computer.service.ts
+++ src/app/services/computer.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Computer } from '../models/computer.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ComputerService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  computerURL = 'http://localhost:3000/computers';
+
+  getAllComputer(): Observable<any> {
+    return this.http.get(this.computerURL);
+  }
+
+  getComputerById(id: number): Observable<Computer> {
+    return this.http.get<Computer>(`${this.computerURL}/${id}`);
+  }
+
+  createComputer(newComputer: Partial<Computer>): Observable<Computer> {
+    return this.http.post<Computer>(this.computerURL, newComputer);
+  }
+
+  updateComputer(computer: Computer): Observable<Computer> {
+    return this.http.put<Computer>(
+      `${this.computerURL}/${computer.id}`,
+      computer
+    );
+  }
+  deleteComputer(computer: Computer): Observable<any> {
+    return this.http.delete(`${this.computerURL}/${computer.id}`);
+  }
 }

b643f91442ab8eab5da5dff5b76c49305ae677c4
Thanh Trung <trung9x.dev@gmail.com>
2020-04-11 21:04:20 +0700

    computer forms

--- src/app/components/computers/computer-forms/computer-forms.component.ts
+++ src/app/components/computers/computer-forms/computer-forms.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Computer } from '../../../models/computer.model';
+import { ComputerService } from '../../../services/computer.service';
 
 @Component({
   selector: 'app-computer-forms',
   templateUrl: './computer-forms.component.html',
-  styleUrls: ['./computer-forms.component.css']
+  styleUrls: ['./computer-forms.component.css'],
 })
 export class ComputerFormsComponent implements OnInit {
+  @ViewChild('btnCloseModal') btnCloseModal;
+  @Output() computerSubmit = new EventEmitter();
 
-  constructor() { }
+  computer: Computer;
+  computerForms: FormGroup;
+  computerStatus = [
+    { val: 'on', name: 'Chưa sử dụng' },
+    { val: 'off', name: 'Đang sử dụng' },
+    { val: 'repair', name: 'Đang sửa chữa' },
+  ];
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.initForms();
   }
 
+  initForms() {
+    this.computerForms = new FormGroup({
+      /* computerId: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('^[0-9]*$'),
+        ])
+      ), */
+      computerId: new FormControl({ value: '', disabled: true }, [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
+      computerLocation: new FormControl('', Validators.required),
+      computerStatus: new FormControl(
+        this.computerStatus[0],
+        Validators.required
+      ),
+    });
+  }
+
+  get field() {
+    return this.computerForms.controls; //<span *ngIf="field.computerId.errors.required"> Không được trống.</span>
+  }
+
+  /* get computerId() {
+    return this.computerForms.get('computerId'); //<span *ngIf="computerId.errors.required"> Không được trống.</span>
+  } */
+
+  onReset() {
+    this.computerForms.reset();
+  }
+
+  onSubmit() {
+    this.computerSubmit.emit();
+    this.btnCloseModal.nativeElement.click();
+  }
+
+  submit(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+  }
 }

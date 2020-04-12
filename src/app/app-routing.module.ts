fed38e11194f6322673fb1163fe2ce8efb77cbc5
Thanh Trung <trung9x.dev@gmail.com>
2020-04-12 21:58:56 +0700

    delete computer, customer and service: create&edit --> update app-routing.module, app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+
 import { ComputerListComponent } from './components/computers/computer-list/computer-list.component';
+import { ComputerFormsComponent } from './components/computers/computer-forms/computer-forms.component';
+
 import { CustomerListComponent } from './components/customers/customer-list/customer-list.component';
+import { CustomerFormsComponent } from './components/customers/customer-forms/customer-forms.component';
+
 import { ServiceListComponent } from './components/services/service-list/service-list.component';
-import { ComputerCreateComponent } from './components/computers/computer-create/computer-create.component';
-import { ComputerEditComponent } from './components/computers/computer-edit/computer-edit.component';
-import { CustomerCreateComponent } from './components/customers/customer-create/customer-create.component';
-import { CustomerEditComponent } from './components/customers/customer-edit/customer-edit.component';
-import { ServiceCreateComponent } from './components/services/service-create/service-create.component';
-import { ServiceEditComponent } from './components/services/service-edit/service-edit.component';
+import { ServiceFormsComponent } from './components/services/service-forms/service-forms.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -18,24 +18,27 @@ const routes: Routes = [
     path: 'computers',
     component: ComputerListComponent,
     children: [
-      { path: 'create', component: ComputerCreateComponent },
-      { path: 'edit', component: ComputerEditComponent },
+      { path: 'create', component: ComputerFormsComponent },
+      { path: 'edit/:id', component: ComputerFormsComponent },
+      { path: 'delete/:id', component: ComputerListComponent },
     ],
   },
   {
     path: 'customers',
     component: CustomerListComponent,
     children: [
-      { path: 'create', component: CustomerCreateComponent },
-      { path: 'edit', component: CustomerEditComponent },
+      { path: 'create', component: CustomerFormsComponent },
+      { path: 'edit/:id', component: CustomerFormsComponent },
+      { path: 'delete/:id', component: CustomerListComponent },
     ],
   },
   {
     path: 'services',
     component: ServiceListComponent,
     children: [
-      { path: 'create', component: ServiceCreateComponent },
-      { path: 'edit', component: ServiceEditComponent },
+      { path: 'create', component: ServiceFormsComponent },
+      { path: 'edit/:id', component: ServiceFormsComponent },
+      { path: 'delete/:id', component: ServiceListComponent },
     ],
   },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,18 +16,12 @@ import { DetailsComponent } from './components/details/details.component';
 
 import { ComputerListComponent } from './components/computers/computer-list/computer-list.component';
 import { ComputerFormsComponent } from './components/computers/computer-forms/computer-forms.component';
-import { ComputerCreateComponent } from './components/computers/computer-create/computer-create.component';
-import { ComputerEditComponent } from './components/computers/computer-edit/computer-edit.component';
 
 import { CustomerListComponent } from './components/customers/customer-list/customer-list.component';
 import { CustomerFormsComponent } from './components/customers/customer-forms/customer-forms.component';
-import { CustomerCreateComponent } from './components/customers/customer-create/customer-create.component';
-import { CustomerEditComponent } from './components/customers/customer-edit/customer-edit.component';
 
 import { ServiceListComponent } from './components/services/service-list/service-list.component';
 import { ServiceFormsComponent } from './components/services/service-forms/service-forms.component';
-import { ServiceCreateComponent } from './components/services/service-create/service-create.component';
-import { ServiceEditComponent } from './components/services/service-edit/service-edit.component';
 
 import { UseComputerComponent } from './components/reg-to-use/use-computer/use-computer.component';
 import { UseServiceComponent } from './components/reg-to-use/use-service/use-service.component';
@@ -37,16 +31,10 @@ import { UseServiceComponent } from './components/reg-to-use/use-service/use-ser
     AppComponent,
     ComputerListComponent,
     ComputerFormsComponent,
-    ComputerCreateComponent,
-    ComputerEditComponent,
     CustomerListComponent,
     CustomerFormsComponent,
-    CustomerCreateComponent,
-    CustomerEditComponent,
     ServiceListComponent,
     ServiceFormsComponent,
-    ServiceCreateComponent,
-    ServiceEditComponent,
     UseComputerComponent,
     UseServiceComponent,
     HomeComponent,
